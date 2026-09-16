2de2abb229acedea3513ce483d21750a1705c18a
kaixianzheng <kaixianzheng1216@gmail.com>
2026-09-16 14:20:13 +0800

    config(files): 关闭文档解析中的 OCR 功能

--- backend/app/modules/files/document_parser.py
+++ backend/app/modules/files/document_parser.py
@@ -49,7 +49,7 @@ async def extract_markdown(
                     "from_formats": document_format,
                     "to_formats": "md",
                     "image_export_mode": "placeholder",
-                    "do_ocr": "true",
+                    "do_ocr": "false",
                 },
                 files={"files": (filename, file)},
             )
